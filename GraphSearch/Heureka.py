e438469cc6934c6b8793d560a0e40188ce96aed9
Davide Catania <davidecatania@gmail.com>
2018-04-12 23:08:43 +0200

    Add path highlight when found and display more information

--- GraphSearch/Heureka.py
+++ GraphSearch/Heureka.py
@@ -5,6 +5,7 @@ from GraphUtils import *
 import sys
 import random
 import time
+import re
 
 from mainwindow import Ui_MainWindow
 
@@ -18,18 +19,28 @@ class MainUiClass(QMainWindow, Ui_MainWindow):
         initialize_street_information()
 
     def run_search(self):
+        start = self.sourceEdit.text()
+        destation = self.dstEdit.text()
+
+        print(start)
         result = GraphSearch.GraphSearch2((10, 70), (65, 110))
+        self.map.mark_paths(result)
         self.statusConsole.clear()
-        self.statusConsole.insertPlainText('Directions...\n\n')
         street_info = initialize_street_information()
+        street_string_info = "\n".join(str(a) for a in street_info)
+        self.statusConsole.insertPlainText(street_string_info)
+        self.statusConsole.insertPlainText('\n\nDirections...\n\n')
         total_cost = 0
         for node in result:
             cost = node.stepCost
             total_cost += cost
             msgFormat = 'Walk from {} through {} to get to {}, Cost: {:.2f}\n'
-            self.statusConsole.insertPlainText(msgFormat.format(node.Parent, get_street_name(node.Parent, node.State, street_info), node.State, cost))
-        self.statusConsole.insertPlainText('\nTotal Cost: {:.2f}'.format(total_cost) + '\n')
-        self.statusConsole.insertPlainText('\nRaw Result\n' + str(result) + '\n')
+            self.statusConsole.insertPlainText(msgFormat.format(node.Parent, get_street_name(
+                node.Parent, node.State, street_info), node.State, cost))
+        self.statusConsole.insertPlainText(
+            '\nTotal Cost: {:.2f}'.format(total_cost) + '\n')
+        self.statusConsole.insertPlainText(
+            '\nRaw Result\n' + str(result) + '\n')
 
 
 if __name__ == '__main__':
--- GraphSearch/MapPlotter.py
+++ GraphSearch/MapPlotter.py
@@ -11,16 +11,13 @@ class MapPlotter(QWidget):
     def __init__(self, parent):
         super().__init__()
         self.initUI()
-
-        self.street_info = []
-
-        with open('Paths.txt') as paths:
-            for line in paths:
-                entry = line.rstrip('\n').split(' ')
-                if len(entry) == 5:
-                    self.street_info.append(Street(StreetName=entry[2], 
-                    start=Coordinate(x=int(entry[0]), y=int(entry[1])), 
-                    end=Coordinate(x=int(entry[3]), y=int(entry[4]))))
+        self.street_info = initialize_street_information()
+        self.displace_x = 25
+        self.displace_y = -25
+        self.displace_coord = QPoint(10,0)
+        self.displace_street = QPoint(10, -5)
+        self.scaler = 6.5
+        self.traversed_paths = []
         
     def initUI(self):        
         self.setGeometry(500, 0, 1900, 900)
@@ -30,7 +27,7 @@ class MapPlotter(QWidget):
     def paintEvent(self, event):
         painter = QPainter()
         painter.begin(self)
-        self.draw_point(painter)
+        self.draw_map(painter)
         painter.end()
 
     def get_mid_point(self, p1, p2):
@@ -38,28 +35,38 @@ class MapPlotter(QWidget):
         p_mid_y = (p1.y()+p2.y())/2
         return QPoint(p_mid_x, p_mid_y)
 
-        
-    def draw_point(self, painter):
+    def mark_paths(self, traversed_paths):
+        self.traversed_paths = traversed_paths
+        print('Marked Paths!')
+        print(traversed_paths)
+        self.repaint()
+
+    def draw_map(self, painter):
         pen = QPen()
         pen.setWidth(4)
         pen.setColor(Qt.black)
         pen.setCapStyle(Qt.RoundCap)
-        
         painter.setPen(pen)
         painter.setFont(QFont('Decorative', 8))
-        displace_x = 25
-        displace_y = -25
-        displace_coord = QPoint(10,0)
-        displace_street = QPoint(10, -5)
-        scaler = 6.5
+
         street_name_set = set()
         for i in self.street_info:
-            startPoint = QPoint(i.start.x + displace_x, i.start.y + displace_y)*scaler
-            endPoint = QPoint(i.end.x + displace_x, i. end.y + displace_y)*scaler
+            startPoint = QPoint(i.start.x + self.displace_x, i.start.y + self.displace_y)*self.scaler
+            endPoint = QPoint(i.end.x + self.displace_x, i. end.y + self.displace_y)*self.scaler
             painter.drawLine(startPoint, endPoint)
-            painter.drawText(displace_coord + startPoint, ('(' + str(i.start.x) + ', ' + str(i.start.y) + ')'))
-            painter.drawText(displace_coord + endPoint, ('(' + str(i.end.x) + ', ' + str(i.end.y) + ')'))
+            painter.drawText(self.displace_coord + startPoint, ('(' + str(i.start.x) + ', ' + str(i.start.y) + ')'))
+            painter.drawText(self.displace_coord + endPoint, ('(' + str(i.end.x) + ', ' + str(i.end.y) + ')'))
             if i.StreetName not in street_name_set:
-                painter.drawText(displace_street + self.get_mid_point(startPoint, endPoint), i.StreetName)
+                painter.drawText(self.displace_street + self.get_mid_point(startPoint, endPoint), i.StreetName)
                 street_name_set.add(i.StreetName)
+
+        pen.setColor(Qt.red)
+        painter.setPen(pen)
+
+        for j in self.traversed_paths:
+            start = Coordinate(j.Parent[0], j.Parent[1])
+            end = Coordinate(j.State[0], j.State[1])
+            startPoint = QPoint(start.x + self.displace_x, start.y + self.displace_y)*self.scaler
+            endPoint = QPoint(end.x + self.displace_x, end.y + self.displace_y)*self.scaler
+            painter.drawLine(startPoint, endPoint)
             
