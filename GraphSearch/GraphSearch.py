8d2942cadd97b322683a19b943ed36ae4bd7aa8a
AlessandroCatania <alessandrocatania1@gmail.com>
2018-04-22 17:33:19 +0200

    A* Algorithm - Added Path Cost Reevaluation

--- GraphSearch/GraphSearch.py
+++ GraphSearch/GraphSearch.py
@@ -3,6 +3,7 @@ from collections import *
 from PyQt5.QtWidgets import QApplication
 from MapPlotter import MapPlotter
 from GraphUtils import *
+from copy import deepcopy
 import sys
 
 Vertices = [] # Empty List
@@ -19,7 +20,6 @@ for i in readPaths('Paths.txt'):
     e = EdgeLabel(streetStretch=obj[1], Coordinate=edgeCoordinate, stepCost=g)
     Edge.append(e)
     Graph[parentCoordinate].append(e)
-
 
 def expand2(node):
     childNodes = []
@@ -41,54 +41,82 @@ def pathReconstruction(initState,currState,List,solutionPath):
     pathReconstruction(initState,currNode.Parent,List,solutionPath)
 
 def calcPathcost(initState,currState,List,pathCost):
-    for node in List:
+    tempList = List
+    if currState == initState or len(tempList) == 0:
+        return 0
+    for node in tempList:
         if node.State == currState:
-            pathCost = node.stepCost + pathCost
+            pathCost = node.stepCost
             currNode = node
             parentState = node.Parent
-    if currState == initState:
-        return
-    calcPathcost(initState, parentState, List, pathCost)
-    return pathCost
+            tempList.remove(currNode)
+    return pathCost + calcPathcost(initState, parentState, tempList, pathCost)
 
+def updateParent(List,currentNode):
+    for node in List:
+        nodeOb = node[0]
+        if nodeOb.State == currentNode.State:
+            nodeOb.Parent = currentNode.Parent
+            nodeOb.stepCost = calcCost(nodeOb.Parent,currentNode.State)
+    return
+
+def reevalPathcost(traversedNodes,initialState,currChild):
+    prevPathCost = 0
+    newPathCost = 0
+    nodes = deepcopy(traversedNodes)
+    prevPathCost = calcPathcost(initialState, currChild.State, nodes, prevPathCost)
+    newPathCost = calcPathcost(initialState, currChild.Parent, nodes, newPathCost) + currChild.stepCost
+    if prevPathCost > newPathCost:
+        return True
+    else:
+        return False
+        frontierSet.remove(child.State)
 
 def GraphSearch2(initialState, goalState):
     frontier = [] # Will contain the nodes in the frontier
     frontierSet = [] # Will contain the current state in the frontier
-    pathCost=0
-    fCost=0
-    initNode = Node(Parent=initialState, State=initialState, stepCost=0)
-    solutionPath = [] # Will contain the solution path
-    frontier.append(Frontier(nodeObj=initNode, cost=fCost)) # This frontier holds a list of nodes
+    initNode = Node(Parent=initialState, State=initialState, stepCost=0) # Initial State - Points to itself - Parent is itself
+    frontier.append(Frontier(nodeObj=initNode, cost=0)) # This frontier holds a list of nodes
     frontierSet.append(initialState) # This is the same as the frontier but just holds a list of states (makes it easier in IF statements to compare states and not nodes)
     exploredSet =[] # Will contain a list of visited states (makes it easier in IF statements to compare states and not nodes)
     pathTrail = []  # Contains a list of visited NODES (used to retrieve the solution path)
+    everyNode = [] # Keeping track of every node in the graph
+    solutionPath = [] # Will contain the solution path
     while len(frontier) > 0:
         currNode = (frontier.pop(0))[0] # Visit the node having top priority
+        frontierSet.remove(currNode.State) # Same as above (remove from frontier)
         if currNode.State == goalState: # Goal Test stage
             pathTrail.append(currNode) # Add goal node to current path
             pathReconstruction(initialState, goalState, pathTrail, solutionPath) # Reconstruct solution path using trail
-            print("SUCCESS!")
             return solutionPath
         pathTrail.append(currNode) # Add to explored
+        if currNode not in everyNode: # Just to avoid repeated nodes
+            everyNode.append(currNode)
         exploredSet.append(currNode.State) # Add to explored
         children = expand2(currNode) # Get the children of the current node
         for child in children:
+            if child.State in frontierSet: #Check if we need to update the path cost of any node (a shorter path was found) and hence parent
+                if reevalPathcost(deepcopy(everyNode), initialState, deepcopy(child)) == True:
+                    updateParent(frontier,deepcopy(child))
             if child.State not in frontierSet and child.State not in exploredSet:
-                test = pathTrail
-                test.append(child)
-                pathCost = calcPathcost(initialState,child.State,test,pathCost) # Get path cost to current node using parent pointers - IMP to use this approach since path cost may change during the search
-                h = calcCost(child.State,goalState)  # Get heuristic cost
-                f = h + pathCost # f = g + h
+                pathCost = 0
+                pathTrail.append(child)
+                if child not in everyNode:
+                    everyNode.append(child)
+                pathCost = calcPathcost(initialState,child.State,deepcopy(pathTrail),pathCost) # Get path cost to current node using parent pointers - IMP to use this approach since path cost may change during the search
+                f = calcCost(child.State,goalState) + pathCost # f = g + h
                 frontier.append(Frontier(nodeObj=child,cost=f))
                 frontierSet.append(child.State)
         frontier = sorted(frontier, key=lambda cost: cost[1])  # Sorting according to f cost
-    print("FAIL")
     return "Fail"
 
 
 if __name__ == '__main__':
     start = (10,70)
     end = (65,110)
-    # GraphSearch2(start, end)
+    GraphSearch2(start, end)
     # print_directions2(GraphSearch2(start,end), end)
+
+
+
+
